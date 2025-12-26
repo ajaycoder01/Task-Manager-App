c3a2c4b42b99047dde93a56c98d6cfd8fe03a8d4
ajaycoder01 <ajaywebmaster01@gmail.com>
2025-12-26 19:23:14 +0530

    Create uploads directory on server startup
    Add upload directory creation if it doesn't exist

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require("fs");
 const path = require('path');
 const connectDB = require('./config/db');
 
@@ -11,6 +12,11 @@ const reportRoutes = require('./routes/reportRoutes');
 require('dotenv').config();
 
 const app = express();
+
+const uploadDir = path.join(__dirname, "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
 
 // Middleware to handle CORS
 app.use(
